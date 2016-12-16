6c5bfb97adbf797e3789f3b7ceba7b66c93a3353
yzhan189 <yzhan189@illinois.edu>
2016-12-16 15:42:12 -0600

    updated status

--- core.clj
+++ core.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str])
   (:gen-class))
 
-(def the-map  ;  15 rooms   14 unconditional objs + 3 conditional objs
+(def the-map  ;  10 static rooms + 5 dynamic rooms   13 unconditional objs + 4 conditional objs
   {:foyer {:desc "The walls are freshly painted but do not have any pictures.
 You get the feeling it was just created for a game or something."
            :title "in the foyer"
@@ -12,15 +12,17 @@ You get the feeling it was just created for a game or something."
            :contents #{:raw-egg }}
    :grue-pen {:desc "It is very dark.  You are about to be eaten by a grue. Are you going to fight? "
               :title "in the grue pen"
-              :hint "Do you know that grue loves raw eggs?"
+              :hint "Don't you know that grue loves raw eggs?"
               :dir {:north :foyer :downstairs :basement :south :truth-room}
               };alter it so that grue doesn't eat you
    :basement { :title "in the basement"
+               :desc "Something is glowing in the dark."
                :hint "Seems like there is something on the ground."
                :dir {:upstairs :grue-pen}
                :contents #{:spirit3 }}
-   :DNE {:title "in The Room Does Not Exist"
-         :hint "Stay."
+   :DNE {:desc "Full of NULL pointers and Segment Faults!!! "
+         :title "in the room DoesNotExist"
+         :hint "STAY IN MY NIGHTMARE."
          :dir {:south :gallery :east :foyer}}
    :kitchen {:title "in kitchen"
              :desc "It is a clean and tidy full kitchen, you may have a decent meal here!"
@@ -28,45 +30,54 @@ You get the feeling it was just created for a game or something."
              :dir {:south :lab :west :foyer}
              :contents #{:cooked-egg }}
    :gallery {:title "in gallery"
-             :hint "Seems like there is something on the ground."
+             :hint "You can pick something up."
+             :desc "A piece of grue lives here."
              :dir {:north :DNE}
              :contents #{:spirit1 }}
    :lab {:title "in lab"
          :hint "Seems like there is something on the ground."
          :dir {:north :kitchen}
+         :desc "You can create somthing here."
          :contents #{:beaker}}
    :truth-room {:title "in truth-room"
                 :hint "Seems like there is something on the ground."
                 :dir {:north :grue-pen}
+                :desc "You can ask for truth."
                 }
 
    :bedroom {:title "in bedroom"
-            :hint "Seems like there is something on the ground."
+            :hint "There is a piece of grue under the bed."
             :dir {:west :study-room :east :bar :south :balcony}
+            :desc "Do not sleep on the bed."
             :contents #{:spirit2 }}
    :ball-room {:title "in ball-room"
                :hint "Seems like there is something on the ground."
+               :desc "Shut up and listen to the whisper."
                :dir {:south :study-room :east :mirror-room}
                :contents #{:spell1 }}
    :bath-room {:title "in bath-room"
                :hint "Seems like there is something on the ground."
                :dir {:west :mirror-room :south :bar}
+               :desc "Shut up and listen to the whisper."
                :contents #{:spell2 }}
    :study-room {:title "in study-room"
                 :hint "Seems like there is something on the ground."
                 :dir {:north :ball-room :east :bedroom}
+                :desc "Shut up and listen to the whisper."
                 :contents #{:spell3 }}
    :bar {:title "in bar"
          :hint "Seems like there is something on the ground."
          :dir {:west :bedroom :north :bath-room}
+         :desc "Shut up and listen to the whisper."
          :contents #{:spell4 }}
    :mirror-room {:desc "It is a room full of mirrors.
 Be careful with the direction you choose! "
                  :title "in mirror-room"
-                 :hint "Seems like there is something on the ground."
+                 :hint "Opposite directions and useless thing on the ground."
                  :dir {:west :bath-room :east :ball-room :downstairs :foyer}
                  :contents #{:broken-mirrors}} ;dir flipped
    :balcony {:title "in the balcony"
+             :desc "It is very large."
              :hint "As the game developper, I'm telling you that this is the exit! "
              :dir {:north :bedroom}
              :contents #{:Patronus }}
@@ -86,15 +97,36 @@ Be careful with the direction you choose! "
     (if (empty? inventory)
       (print (str "nothing"))
       (doseq [ v inventory] (print( str v " "))) )
-    (do (println ".") )
+    (do (println ". ") )
     player))
 
+(defn soul [themap player]
+  (do (if (contains? (player :inventory) :grue-soul)
+        (update-in themap [:grue-pen] assoc :desc (str "The spirit of grue glows in dark. "))
+        (do themap))))
 
-(defn status [player]  ;status associate
+(defn beaker [themap player]
+  (do (if (contains? (player :inventory) :beaker)
+        (update-in themap [:lab] assoc :contents #{}  )
+        (do themap))))
+(defn broken-mirrors [themap player]
+  (do (if (contains? (player :inventory) :broken-mirrors)
+        (update-in themap [:mirror-room] assoc :contents #{}  )
+        (do themap))))
+(defn raw-egg [themap player]
+  (do (if (contains? (player :inventory) :raw-egg)
+        (update-in themap [:foyer] assoc :contents #{}  )
+        (do themap))))
+(defn cooked-egg [themap player]
+  (do (if (> (player :energy) 0 ) ;cooked
+        (update-in themap [:kitchen] assoc :contents #{:cooked-egg}  )
+        (do themap))))
+
+(defn status [player themap]  ;status associate
   (let [location (player :location)]
-    (print (str "You are " (-> the-map location :title) ". "))
+    (print (str "You are " (-> themap location :title) ". \n"))
     (when-not ((player :seen) location) ;when enter from other room
-      (print (-> the-map location :desc)))
+      (print (-> themap location :desc)))
     (update-in player [:seen] #(conj % location))))
 
 (defn to-keywords [commands]
@@ -110,9 +142,9 @@ Be careful with the direction you choose! "
 
 
 
-(defn pickup [player]
+(defn pickup [player themap]
   (let [location (player :location)
-        items (->> the-map location :contents )
+        items (->> themap location :contents )
         item (first items)]
     (if (nil? item)
       (do (println "There is nothing to pick up.") player)
@@ -136,17 +168,17 @@ Be careful with the direction you choose! "
   (do (println "Try: look -- see where you are")
       (println "     check -- see what you have")
       (println "     north, south, east, west, upstairs, downstairs -- move around")
-      (println "     pickup, use -- do something with objects")
+      (println "     pickup -- receive objects")
       (println "     hint, quit, help -- game options")
       (if (< (player :grue-love) 1)
-        (do (println "(Hidden actions are not shown.)"))
+        (do (println "(Hidden actions are not shown. )"))
         (do (println "The more grue loves you, the more tips you will get.")
             (if (> (player :grue-love) 1)
               (do
                 (println "     fight -- your energy decremetns")
                 (println "     feed -- grue-love increments")
-                (println "     cook -- your energy increments")
-                (println "     mix -- use beaker to mix pieces of spirits")
+                (println "     cook -- generate cooked food and your energy increments")
+                (println "     mix -- use beaker to produce pieces of spirits")
                 (if (> (player :grue-love) 2)
                   (do  (println "     ask -- ask grue in truth room to know the correct spell")
                        (println "     shout -- shout correct spell on balcony and get Patronus")
@@ -186,23 +218,28 @@ Be careful with the direction you choose! "
   (let [location (player :location)]
     (if  (= location :kitchen )
       (do (if (contains? (player :inventory) :raw-egg)
-            (do (println "Since there is no other food in kitchen, you just have some boiled eggs.")
+            (do (println "No other food in kitchen, so you boil some eggs. Go pick up cooked eggs. ")
                 (update-in player [:energy] inc ) )
             (do (println "Unfortunately, there is no food left.") player) ) )
 
       (do (println "Come on, yall cannot cook outside kitchen.") player) )
     )) ;set energy to 1
 
+
+
 (defn feed [player]
   (let [location (player :location)]
     (if  (= location :grue-pen )
-      (do (if (contains? (player :inventory) :raw-egg)
-            (do (if (> (player :grue-love) 0)
-                  (do (println "Grue loves you, go for its spirit. ")player)
-                  (do (print (player :grue-love))player))
-                (println "Grue loves you more.")
-                (update-in player [:grue-love] inc ))
-            (do (println "Seriously? Are you going to feed grue with yourself?") player)))
+      (do (if (contains? (player :inventory) :grue-soul)
+            (do (println "Grue has soul now.")
+               player)
+            (do (if (contains? (player :inventory) :raw-egg)
+                  (do (if (> (player :grue-love) 0)
+                        (do (println "Grue loves you, go for its spirit. ")player)
+                        (do (print (player :grue-love))player))
+                      (println "Grue loves you more.")
+                      (update-in player [:grue-love] inc ))
+                  (do (println "Seriously? Are you going to feed grue with yourself?") player)))))
       (do (println "No one is there to be fed.")player))
     )) ;grue disapper , soul appears
 
@@ -210,8 +247,8 @@ Be careful with the direction you choose! "
   (let [location (player :location)]
     (if  (= location :grue-pen )
       (do (println "You lose the fight. (for sure...) ")
-          (println "And you got tired.")
-          (update-in player [:energy] dec ))
+          (println "And grue hates you more.")
+          (update-in player [:grue-love] dec ))
       (do (println "No one is there to fight.") player))
     ))
 
@@ -250,7 +287,7 @@ Be careful with the direction you choose! "
       (do (print (str content "... (echo) "  ) ) player ))
   ) )
 
-(defn respond [player command] ; 19 commands
+(defn respond [player command themap] ; 19 commands
   (match command
          [:look] (update-in player [:seen] #(disj % (-> player :location)))
          [:check] (check player)
@@ -262,7 +299,7 @@ Be careful with the direction you choose! "
          [:upstairs] (go :upstairs player)
          [:downstairs] (go :downstairs player)
 
-         [:pickup] (pickup player) ;recieve item
+         [:pickup] (pickup player themap) ;recieve item
          [:mix] (mix player)
          [:ask] (ask player)
          [:cook] (cook player)
@@ -284,7 +321,14 @@ Be careful with the direction you choose! "
   [& args]
   (loop [local-map the-map
          local-player adventurer]
-    (let [pl (status local-player)
+    (let [m4 (cooked-egg local-map local-player)
+          m3 (broken-mirrors m4 local-player)
+          m2 (soul m3 local-player)
+          m1 (raw-egg m2 local-player)
+          m  (beaker m1 local-player)
+          pl (status local-player m)
           _  (println "What do you want to do?")
           command (read-line)]
-      (recur local-map (respond pl (to-keywords command))))))
+     ; (print (-> m :lab :contents))
+     ; (print (-> local-player :inventory))
+      (recur m (respond pl (to-keywords command) m )))))
